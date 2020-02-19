4e09666e42ef88b87f9d90879aaef9a5febd5f76
liqianqian <1142970858@qq.com>
2020-02-19 16:03:59 +0800

    提交index.php

--- get_access_key.php
+++ get_access_key.php
@@ -1,7 +1,7 @@
 <?php
-include_once './config/config.php';
+include_once __DIR__.'/config/config.php';
 
-$access_token=file_get_contents('./config/access_token.txt');
+$access_token=file_get_contents(__DIR__.'/config/access_token.txt');
 $access_token=json_decode($access_token,true);
 //刷新access_token
 if(($access_token['expires_in']+$access_token['time'])-time() <=300 ){
@@ -14,11 +14,11 @@ if(($access_token['expires_in']+$access_token['time'])-time() <=300 ){
     if(isset($response['access_token'])){
         $response['time']=time();
         //保存access_token
-        file_put_contents('./config/access_token.txt',json_encode($response));
+        file_put_contents(__DIR__.'/config/access_token.txt',json_encode($response));
     }else{
         //记录错误日志
-        file_put_contents('./log/error_log.txt',$response);
+        file_put_contents(__DIR__.'/log/error_log.txt',$response);
     }
     print_r($response);die;
 }
-echo 'time()'.print_r($access_token);die;
+print_r($access_token);die;
--- index.php
+++ index.php
@@ -1,3 +1,40 @@
 <?php
-$echostr=$_GET['echostr'];
-echo $echostr;
+//微信接口
+    $signature = $_GET["signature"];
+    $timestamp = $_GET["timestamp"];
+    $nonce = $_GET["nonce"];
+
+    $token = 'thisiswechat';
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr, SORT_STRING);
+    $hash_str = sha1(implode($tmpArr));
+
+    if($signature == $hash_str){
+        if(isset($_GET['echostr'])){
+            //用于微信验证token
+            echo $_GET['echostr'];exit;
+        }else{
+            //微信推送的消息
+            $xml = file_get_contents("php://input");
+            //xml转成对象
+            $xmlObj = simplexml_load_string($xml);
+            //关注回复
+            if($xmlObj->MsgType == 'event' && $xmlObj->Event == 'subscribe'){
+                 $info =  "<xml>
+                          <ToUserName><![CDATA[".$xmlObj->FromUserName."]]></ToUserName>
+                          <FromUserName><![CDATA[".$xmlObj->ToUserName."]]></FromUserName>
+                          <CreateTime>time()</CreateTime>
+                          <MsgType><![CDATA[text]]></MsgType>
+                          <Content><![CDATA[".您好，欢迎关注！."]]></Content>
+                        </xml>";
+                file_put_contents("./error_log.txt",json_encode($info));
+                echo $info;
+            }
+        }
+    }
+
+
+//写文件里
+//file_put_contents("./log.txt",json_encode($_GET)); //微信发来的数据
+//file_put_contents("./log1.txt",file_get_contents("php://input")); //接收原始xml数据或json数据流
+
